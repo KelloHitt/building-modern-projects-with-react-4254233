13d75c6229b1b44a4e76f5638ad8eef4302c0ecb
KelloHitt <venus.cert@gmail.com>
2026-06-01 18:43:42 +1200

    Installed webpack

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,1 +1,12 @@
-console.log('This is the React rendering script!');
+import React from 'react';
+import {createRoot} from 'react-dom';
+
+const App = () => {
+  return <h1>This is a React Script!</h1>
+}
+
+// Getting the root div element from index.html where the React app will render into
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
